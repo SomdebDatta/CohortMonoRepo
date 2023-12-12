475004c815c9e0c82d7e67d5d57b9efffbc54b5c
Somdeb Datta <somdeb.datta@siemens.com>
2023-12-12 16:00:20 +0530

    adding main funcs

--- Week2Assignment/01-async-js/easy/counter-2.js
+++ Week2Assignment/01-async-js/easy/counter-2.js
@@ -4,11 +4,14 @@ Without using setInterval, try to code a counter in Javascript. There is a hint 
 
 (Hint: setTimeout)
 */
-console.log("Hello World!");
-console.log("Counter will start now");
-let counter = 0;
-setInterval(function(){
-    console.log(`Counter is at ${counter}. Time - ${new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata', hour12: true })}`);
-    counter += 1;
-}, 2000);
+function main() {
+    console.log("Hello World!");
+    console.log("Counter will start now");
+    let counter = 0;
+    setInterval(function(){
+        console.log(`Counter is at ${counter}. Time - ${new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata', hour12: true })}`);
+        counter += 1;
+    }, 2000);
+}
+main();
 
--- Week2Assignment/01-async-js/easy/counter-3.js
+++ Week2Assignment/01-async-js/easy/counter-3.js
@@ -4,19 +4,23 @@ Without using setInterval, try to code a counter in Javascript. There is a hint 
 
 (Hint: setTimeout)
 */
-console.log("Hello World!");
-console.log("Counter will start now");
-let counter= 0;
-
-function counter_func() {
+function main() {
+    console.log("Hello World!");
+    console.log("Counter will start now");
+    let counter= 0;
     
-    setTimeout(function(){
-        counter += 1;
-        console.log(`Counter is at ${counter}. Time - ${new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata', hour12: true })}`);
-        counter_func();
-    }, 2000);
-    console.log("This is the while loop log");
-    
-    
+    function counter_func() {
+        
+        setTimeout(function(){
+            counter += 1;
+            console.log(`Counter is at ${counter}. Time - ${new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata', hour12: true })}`);
+            counter_func();
+        }, 2000);
+        console.log("This is the while loop log");
+        
+        
+    }
+    counter_func();
 }
-counter_func();
+
+main();
