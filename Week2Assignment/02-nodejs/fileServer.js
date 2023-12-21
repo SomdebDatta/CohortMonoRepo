35840f18b766530d3cc73e770231b33cf50899b4
SomdebDatta <somdebdatta@gmail.com>
2023-12-21 18:12:25 +0530

    Solved file server

--- Week2Assignment/02-nodejs/fileServer.js
+++ Week2Assignment/02-nodejs/fileServer.js
@@ -17,5 +17,41 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+const base_path = "./files/";
+
+app.get("/files", (req, res) => {
+  const file_list = [];
+  fs.readdir(base_path, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory`, err);
+    }
+    
+    console.log("Files in the directory are...");
+    files.forEach(file => {
+      file_list.push(file);
+      console.log(file);
+    })
+    res.send(file_list);
+  })
+})
+
+app.get("/file/:filename", (req, res) => {
+  const filename = req.params.filename;
+  file_path = base_path + filename;
+
+  fs.readFile(file_path, (err, data) => {
+    if (err) {
+      res.status(404).send("File not found");
+    }
+
+    res.send(data);
+  })
+})
+
+app.all("*", function(req, res) {
+  res.status(404).send("Route not found")
+})
+
+// app.listen(3000);
 
 module.exports = app;
