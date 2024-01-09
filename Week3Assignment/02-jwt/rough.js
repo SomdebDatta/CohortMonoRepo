57311ae85a4d4826d42ca92d7e3a521e300868a0
SomdebDatta <somdebdatta@gmail.com>
2024-01-09 11:43:28 +0530

    understanding jwt

--- Week3Assignment/02-jwt/rough.js
+++ Week3Assignment/02-jwt/rough.js
@@ -1,3 +1,19 @@
 jwt = require("jsonwebtoken");
 
-console.log(jwt.decode("token"));
+const my_str = "saideepulovessomjulovesdeepu";
+const key = "sex";
+
+// console.log(jwt.decode("token"));
+
+token = jwt.sign(my_str, key);
+
+console.log(token);
+
+console.log(jwt.verify(token, key));
+console.log(jwt.decode(token));
+
+// prev_token= "eyJhbGciOiJIUzI1NiJ9.c2FpZGVlcHVsb3Zlc3NvbWp1bG92ZXNkZWVwdQ.zkpjvgBcQsIR7s3upzL2anP057Dy__kZVgb3mcUV7gA" -> "shhh"
+// new_token = "eyJhbGciOiJIUzI1NiJ9.c2FpZGVlcHVsb3Zlc3NvbWp1bG92ZXNkZWVwdQ.jZGXSNbkFYVhtsIMarI0n51h6vf6KwoGHmau1aAXJNw" -> "sex"
+
+
+// token = {"saideepulovessomjulovesdeepu" + "shhh"->128bitenocding}
