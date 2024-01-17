bafb6e31d30cc5ccfa48f884b9f519a3a29798ea
SomdebDatta <somdebdatta@gmail.com>
2024-01-17 12:38:54 +0530

    useMemo example

--- Week6Class/react_useMemo/src/App.jsx
+++ Week6Class/react_useMemo/src/App.jsx
@@ -1,27 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
-  const [num, setNum] = useState(0);
+  const [num, setNum] = useState(1);
 
   const [counter, setCounter] = useState(0);
 
-  function CalculateFactorial({num}) {
-    let sum = 0;
-    for (let i=1; i<=num; i++) {
+  let sum = useMemo(() => {
+    let sum = 1;
+    for(let i=2; i<=num; i++) {
       sum+=i;
     }
-    return <div>
-      Sum is {sum}
-    </div>
-  }
+    return sum;
+  }, [num]);
+
+  
 
   return (
     <>
     <div>
-      <input id="num" placeholder="Enter Number" onInput={function() {
-        setNum(parseFloat(document.getElementById("num").value))
+      <input id="num" placeholder="Enter Number" onChange={function(x) {
+        setNum(parseFloat(x.target.value))
       }}></input>
-      <CalculateFactorial num={num} />
+      <br />
+      Sum from 1 to {num} is {sum}
+      <br/>
       <button onClick={function() {
         setCounter(counter+1);
       }}>Counter {counter}</button>
