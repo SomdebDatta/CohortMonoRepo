91cc083ccf1d15f62c67843eb21b9d1625c309b2
SomdebDatta <somdebdatta@gmail.com>
2024-01-17 13:37:59 +0530

    useCallback example

--- Week6Class/useCallback/src/App.jsx
+++ Week6Class/useCallback/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState, useCallback, memo } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const logSomething = useCallback(() => {
+    console.log("Child clicked");
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ButtonComponent inputFunction={logSomething}/>
+      <button onClick={() => {
+        setCount(count+1);
+      }}>Click me {count}</button>
     </>
   )
 }
+
+const ButtonComponent = memo(({inputFunction}) => {
+  console.log("Child render");
+
+  return <div>
+    <button>Button clicked</button>
+  </div>
+})
 
 export default App
