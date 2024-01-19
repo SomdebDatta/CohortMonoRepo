2355657c7fc546aad046a11087429d3682411b82
SomdebDatta <somdebdatta@gmail.com>
2024-01-19 23:36:49 +0530

    useNavigate

--- Week7Class/week_7_1/src/App.jsx
+++ Week7Class/week_7_1/src/App.jsx
@@ -1,5 +1,33 @@
+import { Dashboard } from "./components/Dashboard"
+import { Landing } from "./components/Landing"
+
+import { BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
+
 function App() {
-  
+
+  return (
+    <BrowserRouter>
+    <Appbar />
+      <Routes>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/" element={<Landing />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+
+  return (
+  <div style={{background: "blue"}}>
+    <button onClick={() => {
+      navigate("/");
+      }}>Landing button</button>
+    <button onClick={() => {
+      navigate("/dashboard");
+      }}>Dashboard button</button>
+  </div>)
 }
 
 export default App
