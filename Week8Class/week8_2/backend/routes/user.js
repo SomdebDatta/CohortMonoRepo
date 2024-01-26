7c519de7ecccaf1e5b791f77139f3e2f71eb8eac
SomdebDatta <somdebdatta@gmail.com>
2024-01-26 23:24:08 +0530

    elegant solution

--- Week8Class/week8_2/backend/routes/user.js
+++ Week8Class/week8_2/backend/routes/user.js
@@ -7,13 +7,23 @@ const { JWT_SECRET } = require('../config');
 
 const userRouter = express.Router();
 
-const email_validator = zod.string().email();
+const signupBody = zod.object({
+    username: zod.string().email(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string()
+})
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
 
 userRouter.post('/signup', async (req, res) => {
 
-    const usernameValidation = email_validator.safeParse(req.body.username);
+    const success = signupBody.safeParse(req.body.username);
 
-    if (!usernameValidation.success) {
+    if (!success) {
         res.status(411).json({message: 'Incorrect input.'});
     }
 
@@ -22,22 +32,40 @@ userRouter.post('/signup', async (req, res) => {
     if (response) {
         res.status(411).json({message: 'Email already taken'});
     }
-    else {
-        await User.create({
-            username: req.body.username,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: req.body.password
-        });
+    const user = await User.create({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password
+    });
 
-        const token = jwt.sign(req.body.username, JWT_SECRET);
-        res.json({
-            message: 'User created successfully',
-            token: token
-        });
-
-
+    const token = jwt.sign(req.body.username, JWT_SECRET);
+    res.json({
+        message: 'User created successfully',
+        token: token
+    });
     }
+)
+
+userRouter.post('signin', async (req, res) => {
+    const success = signinBody.safeParse(req.body);
+
+    if(!success) {
+        res.status(411).json({message: 'Error logging in'});
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (!user) {
+        res.status(411).json({message: 'Error while logging in'});
+    }
+
+    const token = jwt.sign({username}, JWT_SECRET);
+
+    res.json({token: token});
 })
 
 module.exports = { userRouter };
