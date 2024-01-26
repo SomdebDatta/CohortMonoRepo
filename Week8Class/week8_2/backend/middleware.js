69a72cfdfa02f96b0999b57e677041ddc140888f
SomdebDatta <somdebdatta@gmail.com>
2024-01-26 23:40:19 +0530

    elegant solution

--- Week8Class/week8_2/backend/middleware.js
+++ Week8Class/week8_2/backend/middleware.js
@@ -4,17 +4,21 @@ const { JWT_SECRET } = require('./config');
 
 function authMiddleware(req, res, next) {
     const token = req.headers.authorization;
-    const words = token.split(' ');
-    const jwtToken = words[1];
+    if (!token || !token.startsWith('Bearer ')) {
+        return res.status(403).json({message: 'You are not authenticated.'});
+    }
+    
+    const jwtToken = token.split(' ')[1];
 
-    const decodedToken = jwt.verify(jwtToken, JWT_SECRET);
+    
 
-    if (decodedToken.username) {
+    try {
+        const decodedToken = jwt.verify(jwtToken, JWT_SECRET);
         req.username = decodedToken.username;
         next();
+    } catch(err) {
+        return res.status(403).json({message: 'You are not authenticated.'});
     }
-
-    res.status(403).json({message: 'You are not authenticated.'});
 }
 
 module.exports = authMiddleware;
