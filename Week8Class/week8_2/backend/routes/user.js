ab2c27b83efdada8be611d6621314cf2a33e51cf
SomdebDatta <somdebdatta@gmail.com>
2024-01-27 15:42:38 +0530

    custom filtering

--- Week8Class/week8_2/backend/routes/user.js
+++ Week8Class/week8_2/backend/routes/user.js
@@ -17,11 +17,13 @@ const signupBody = zod.object({
 
 userRouter.post('/signup', async (req, res) => {
 
-    const { success } = signupBody.safeParse(req.body.username);
+    const { success } = signupBody.safeParse(req.body);
 
     if (!success) {
         return res.status(411).json({message: 'Incorrect input.'});
     }
+
+    console.log("zod validated.");
 
     const existingUser = await User.findOne({username: req.body.username});
 
@@ -35,14 +37,13 @@ userRouter.post('/signup', async (req, res) => {
         password: req.body.password
     });
 
-    const userId = user._id
+    const userId = user._id;
 
     const token = jwt.sign({userId}, JWT_SECRET);
-    res.json({
+    return res.json({
         message: 'User created successfully',
         token: token
     });
-    return;
 })
 
 const signinBody = zod.object({
@@ -70,8 +71,7 @@ userRouter.post('/signin', async (req, res) => {
 
     const token = jwt.sign({userId}, JWT_SECRET);
 
-    res.json({token: token});
-    return;
+    return res.json({token: token});
 })
 
 const updateBody = zod.object({
@@ -80,16 +80,32 @@ const updateBody = zod.object({
     lastName: zod.string().optional()
 });
 
-userRouter.put('', authMiddleware, async (req, res) => {
-    const user = await User.updateOne({userId: req.userId}, {}, function(err, docs) {
-        if (err) {
-            console.log(err);
-            res.status(411).json({message: 'Error while updating information'});
-        }
-        else {
-            res.json({message: 'Updated successfully.'})
-        }
-    })
+userRouter.put('/', authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body);
+    if(!success) {
+        return res.json({message: 'Error while updating information.'});
+    }
+    const user = await User.updateOne({_id: req.userId}, req.body);
+    return res.json({message: 'Updated successfully.'});
+})
+
+userRouter.get('/bulk', async (req, res) => {
+    const filter = req.query.filter;
+
+    const users = await User.find({
+        $or: [{firstName: {'$regex': `${filter}`, '$options': 'i'}},
+        {lastName: {'$regex': `${filter}`, '$options': 'i'}}]
+    });
+
+    return res.json({
+        users: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        })
+        )
+    });
 })
 
 module.exports = { userRouter };
