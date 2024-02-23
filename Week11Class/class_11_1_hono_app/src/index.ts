0b30dbafdfd1dcff6dc73ba218ee7019cf918b47
SomdebDatta <somdebdatta@gmail.com>
2024-02-23 19:02:57 +0530

    testing hono app

--- Week11Class/class_11_1_hono_app/src/index.ts
+++ Week11Class/class_11_1_hono_app/src/index.ts
@@ -2,7 +2,12 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
   return c.text('Hello Hono!')
 })
 
